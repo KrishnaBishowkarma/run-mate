23054f10fe7b8631ff41ada08e94ae76a11d8951
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-01-30 17:04:49 +0545

    feat: add RunController with endpoint to fetch all runs

---
+++ src/main/java/com/krishna/runmate/run/RunRepository.java
@@ -0,0 +1,34 @@
+package com.krishna.runmate.run;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class RunRepository {
+
+    private List<Run> runs = new ArrayList<>();
+
+    List<Run> findAll() {
+        return runs;
+    }
+
+    @PostConstruct
+    private void init() {
+        runs.add(new Run(1,
+                "Sunday Morning RUn",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(1),
+                1,
+                Location.OUTDOOR));
+        runs.add(new Run(2,
+                "Monday Evening Run",
+                LocalDateTime.now(),
+                LocalDateTime.now().plusHours(2),
+                2,
+                Location.INDOOR));
+    }
+}
