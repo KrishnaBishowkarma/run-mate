aef09b611b001c79be7182e152bbf64bfdf79824
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-01-30 14:42:46 +0545

    chore: add logging for application startup in main method

--- src/main/java/com/krishna/runmate/RunMateApplication.java
+++ src/main/java/com/krishna/runmate/RunMateApplication.java
@@ -1,13 +1,17 @@
 package com.krishna.runmate;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RunMateApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(RunMateApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(RunMateApplication.class, args);
+		log.info("RunMate application started successfully.");
 	}
-
 }
