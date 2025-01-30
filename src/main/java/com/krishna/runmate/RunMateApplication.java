2e63a139e2d30511861702c0182f6b5e06498b01
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-01-30 15:55:01 +0545

    feat: add Run record, Location enum, and log sample run on startup

--- src/main/java/com/krishna/runmate/RunMateApplication.java
+++ src/main/java/com/krishna/runmate/RunMateApplication.java
@@ -1,9 +1,15 @@
 package com.krishna.runmate;
 
+import com.krishna.runmate.run.Location;
+import com.krishna.runmate.run.Run;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.time.LocalDateTime;
 
 @SpringBootApplication
 public class RunMateApplication {
@@ -13,5 +19,13 @@ public class RunMateApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RunMateApplication.class, args);
 		log.info("RunMate application started successfully.");
+	}
+
+	@Bean
+	CommandLineRunner runner() {
+		return args -> {
+			Run run = new Run(1, "First RUn", LocalDateTime.now(), LocalDateTime.now().plusHours(1), 5, Location.OUTDOOR);
+			log.info("Run: {}", run);
+		};
 	}
 }
---
+++ src/main/java/com/krishna/runmate/run/Location.java
@@ -0,0 +1,5 @@
+package com.krishna.runmate.run;
+
+public enum Location {
+    INDOOR, OUTDOOR
+}
