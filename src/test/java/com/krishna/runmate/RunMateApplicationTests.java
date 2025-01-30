515aa20c3c881edab5f06843c952bb07a742597e
Krishna Bishowkarma <bkkrishna6651@gmail.com>
2025-01-30 13:46:56 +0545

    initial commit

---
+++ src/test/java/com/krishna/runmate/RunMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.krishna.runmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
